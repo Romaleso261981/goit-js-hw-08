6eb55b69df3510d2a89c0dbd4375178366a07a61
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-08-14 13:36:55 +0300

    fix homework 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,19 +1,21 @@
-// Add imports above this line
-// import { galleryItems } from './gallery-items';
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
-  galleryEl: document.querySelector(".gallery"),
+  galleryEl: document.querySelector('.gallery'),
 };
+
+
 const markupHtml = galleryItems
   .map(({ description, original, preview }) => {
     return `<div class="gallery__item">
-    <a class="gallery__link" href="large-image.jpg">
+    <a class="gallery__link" href="${original}">
       <img
         class="gallery__image"
         src="${preview}"
-        data-source="${original}"
+
         alt="${description}"
       />
     </a>
@@ -21,26 +23,10 @@ const markupHtml = galleryItems
   })
   .join("");
 
-// refs.galleryEl.insertAdjacentHTML("afterbegin", markupHtml);
-refs.galleryEl.addEventListener("click", openModal);
-console.log(galleryEl);
+refs.galleryEl.insertAdjacentHTML('afterbegin', markupHtml);
+console.log(refs.galleryEl);
 
-// var lightbox;
-
-function openModal(e) {
-  e.preventDefault();
-  if (e.target.tagName !== "IMG") return;
-  const dataSource = e.target.dataset.source;
-
- lightbox = new SimpleLightbox('.gallery a', { /* options */ });
-  
-  window.addEventListener("keydown", addEventKay);
-}
-
-
-function addEventKay(e) {
-  if (e.code === "Escape") {
-    window.removeEventListener('keydown', addEventKay);
-  }
-}
-
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
