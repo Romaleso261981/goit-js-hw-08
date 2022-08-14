efdb4e80d22d60f114e363fed3d83a07b643d879
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-08-14 12:18:22 +0300

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const refs = {
+  galleryEl: document.querySelector(".gallery"),
+};
+const markupHtml = galleryItems
+  .map(({ description, original, preview }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+  })
+  .join("");
+
+refs.galleryEl.insertAdjacentHTML("afterbegin", markupHtml);
+refs.galleryEl.addEventListener("click", openModal);
+
+var lightbox;
+
+function openModal(e) {
+  e.preventDefault();
+  if (e.target.tagName !== "IMG") return;
+  const dataSource = e.target.dataset.source;
+
+ lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+  
+  window.addEventListener("keydown", addEventKay);
+}
+
+
+function addEventKay(e) {
+  if (e.code === "Escape") {
+    window.removeEventListener('keydown', addEventKay);
+  }
+}
+
